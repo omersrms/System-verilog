89a42ca28fefa20caa7e6fca9805b6a7ce0e6c25
omersrms <96339138+omersrms@users.noreply.github.com>
2021-12-18 22:47:45 +0300

    Add files via upload

---
+++ moore/tb/tb_pattern_moore.sv
@@ -0,0 +1,23 @@
+module tb_pattern_moore ();
+logic clk,A;
+logic B;
+logic [31:0] p=32'b10100100100100100100100010011011;
+
+pattern_moore dut0(clk,A,B);
+always begin
+        clk=0;#5;clk=1;#5;
+    end
+
+initial begin
+        A=0;#20;
+        for(int i=0;i<32;i++) 
+        begin
+            A=p[31];
+            p=p<<1'b1;
+            #10;
+        end
+        #20;
+        $stop;
+    end
+
+endmodule
